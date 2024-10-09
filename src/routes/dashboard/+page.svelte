da04f75d348438425652236df9853f23b71822ff
Shane Curtis <shanecurtis1705@gmail.com>
2024-10-10 11:41:36 +1300

    just added a test toast

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import Button from "$lib/components/ui/button/button.svelte";
+	import { toast } from "svelte-sonner";
+	function handleClick() {
+		toast("yes!");
+	}
 </script>
 
 <div class="flex w-screen h-screen items-center justify-center">
-	<Button>Testing</Button>
+	<Button on:click={handleClick}>Testing</Button>
 </div>
